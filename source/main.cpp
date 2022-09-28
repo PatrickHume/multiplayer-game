abd64cce0b55b953a2d36468725022fd2aae7380
Patrick Hume <patrick.hume@student.manchester.ac.uk>
2022-09-28 16:52:54 +0100

    feat: make pyramid rotate

--- source/main.cpp
+++ source/main.cpp
@@ -107,6 +107,9 @@ int main()
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -122,10 +125,17 @@ int main()
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
 
+        double crntTime = glfwGetTime();
+        if (crntTime - prevTime >= 1 / 60){
+            rotation += 0.5f;
+            prevTime = crntTime;
+        }
+
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 proj = glm::mat4(1.0f);
 
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f,1.0f,0.0f));
         view = glm::translate(view, glm::vec3(0,-0.5f,-2.0f));
         proj = glm::perspective(glm::radians(45.0f),(float)(SCR_WIDTH/SCR_HEIGHT), 0.1f, 100.0f);
 
