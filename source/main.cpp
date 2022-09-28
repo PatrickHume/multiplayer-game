7e31b35699d6cc1172e602f6eacb4b2289891871
Patrick Hume <patrick.hume@student.manchester.ac.uk>
2022-09-28 16:27:43 +0100

    build: include glm

--- source/main.cpp
+++ source/main.cpp
@@ -3,8 +3,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
-
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include"../headers/shaderClass.h"
 #include"../headers/texture.h"
