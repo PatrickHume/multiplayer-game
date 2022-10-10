8e337e746deac560cd68f925ac52d25b2f569cb4
PatrickHume <patrick.hume31@gmail.com>
2022-10-10 22:27:58 +0100

    docs: add comments to screen

--- headers/screen.h
+++ headers/screen.h
@@ -2,16 +2,21 @@
 #define SCREEN_CLASS_H
 #include <GLFW/glfw3.h>
 
+// Screen holds the values related to the window dimensions.
+// It is mostly acessed to find the center of the screen throughout the program.
 class Screen{
     public:
         Screen();
-        static void Resize(GLFWwindow *window);
-        // viewport values
-        static int x, y, width, height;
-        
-        static int frameBufferWidth, frameBufferHeight;
-
-        static int windowWidth, windowHeight;
+        static void recordNewWindowDimensions(GLFWwindow *window);
+        // The viewport width in pixels.
+        static int frameBufferWidth;
+        // The viewport height in pixels.
+        static int frameBufferHeight;
+        // The window width in non-pixel units.
+        static int windowWidth;
+        // The window height in non-pixel units.
+        static int windowHeight;
+        // The aspect ratio of the window.
         static float windowAspect;
     private:
 };
--- source/screen.cpp
+++ source/screen.cpp
@@ -1,19 +1,25 @@
 #include "../headers/screen.h"
+// Re-commented here as well as in 'screen.h' as 
+// intellisence does not equate 'Class:attribute' to 'attribute'.
 
-int Screen::windowWidth      = 800;
-int Screen::windowHeight     = 600;
-int Screen::frameBufferWidth     = 0;
-int Screen::frameBufferHeight    = 0;
+// The window width in non-pixel units.
+int Screen::windowWidth         = 800;
+// The window height in non-pixel units.
+int Screen::windowHeight        = 600;
+// The viewport width in pixels.
+int Screen::frameBufferWidth    = 0;
+// The viewport height in pixels.
+int Screen::frameBufferHeight   = 0;
+// The aspect ratio of the window.
 float Screen::windowAspect = (float)Screen::windowWidth/Screen::windowHeight;
 
-Screen::Screen(){
-    
+// Screen holds the values related to the window dimensions.
+// It is mostly acessed to find the center of the screen.
+Screen::Screen(){   
 };
 
-void Screen::Resize(GLFWwindow *window){
+// Set the values to match the dimensions of the screen given by GLFW.
+void Screen::recordNewWindowDimensions(GLFWwindow *window){
     glfwGetWindowSize(window, &Screen::windowWidth, &Screen::windowHeight);
     glfwGetFramebufferSize(window, &Screen::frameBufferWidth, &Screen::frameBufferHeight);
-    //Screen::frameBufferWidth = Screen::frameBufferHeight * Screen::windowAspect;
-    //int w = height * Screen::windowAspect;           // w is width adjusted for aspect ratio
-    //int left = (width - w) / 2;
 };
