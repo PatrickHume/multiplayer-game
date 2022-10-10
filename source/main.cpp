a2309b8582b2533e6c88e1e4646a996f755c63eb
PatrickHume <patrick.hume31@gmail.com>
2022-10-10 22:28:12 +0100

    docs: add comments to main

--- source/main.cpp
+++ source/main.cpp
@@ -7,107 +7,105 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "../headers/world.h"
 
+// Include gperftools performance profiler if PROFILE is defined.
+// ./showprof.sh will open up the result of the profiler.
 #ifdef PROFILE
 #include <gperftools/profiler.h>
 #endif // PROFILE
 
-#include"../headers/world.h"
-
+// Called when the window is resized, resizes the viewport to match.
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-//void processInput(GLFWwindow *window, Interface &interface, Camera &camera);
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
+    // Initialize and configure GLFW.
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
+    #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+    #endif
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(Screen::windowWidth, Screen::windowHeight, "LearnOpenGL", NULL, NULL);
+    // Create a GLFW window.
+    GLFWwindow* window = glfwCreateWindow(Screen::windowWidth, Screen::windowHeight, "multiplayer-game", NULL, NULL);
     if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
+    {// Window creation error handling.
         glfwTerminate();
-        return -1;
+        throw std::runtime_error("Failed to create GLFW window.");
     }
+    // Set OpenGl current contex to this window.
     glfwMakeContextCurrent(window);
-
+    // Resize the viewport on window resize.
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
+    // Load OpenGL function pointers with glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        throw std::runtime_error("Failed to initialize GLAD");
     }
-
-	// Introduce the window into the current context
-	glfwMakeContextCurrent(window);
-    //glViewport(0, 0, width, height);
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    //https://stackoverflow.com/questions/9264346/problems-outputting-gl-primitiveid-to-custom-frame-buffer-object-fbo
-
+    
+    // Bind the frame buffer to 0, the frame that is drawn to the viewport.
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
+    // Enable depth and stencil testing.
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_STENCIL_TEST);
-    // when both depth and stencil tests pass we use the reference value later
+
+    // Only overwrite the pixel if both depth and stencil tests pass.
     glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
-    Screen::Resize(window);
+    // Record the window dimensions in the screen class.
+    Screen::recordNewWindowDimensions(window);
+
+    // Create the world - this is where most of the game logic happens.
     World world(window);
 
-    // main loop
-    // -----------
+    // Name the output file "output.pprof" and begin profiling if debugging.
     #ifdef PROFILE
     ProfilerStart("output.pprof");
     #endif // PROFILE
+
+    // Run the mainloop until the window is closed by the user.
     while (!glfwWindowShouldClose(window))
     {
+        // Pass the user input to world.
         world.ProcessInput(window);
+        // Update the physics of all objects in world.
         world.Update();
+        // Render the world to the back buffer.
         world.Draw();
-        //textRenderer.renderText(textShader, "This is some sample text", 25.0f, 25.0f, 0.3f, glm::vec3(1.0f, 1.0f, 1.0f));
-      
-		// Swap the back buffer with the front buffer
+
+		// Swap the back buffer with the front buffer and display to the viewport.
 		glfwSwapBuffers(window);
-		// Take care of all GLFW events
+		// Poll all GLFW events.
 		glfwPollEvents();
     }
+
+    // Output the profiling file if debugging.
     #ifdef PROFILE
     ProfilerStop();
     #endif // PROFILE
 
+    // Delete the world.
     world.Delete();
-    	// Delete window before ending the program
+    // Delete the window before ending the program.
 	glfwDestroyWindow(window);
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
+    // Clear all previously allocated GLFW resources.
     glfwTerminate();
+
     return 0;
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+// Resize the viewport on window resize.
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    int w = height * Screen::windowAspect;           // w is width adjusted for aspect ratio
+    // Adjust the width to match the aspect ratio.
+    int w = height * Screen::windowAspect;
     int left = (width - w) / 2;
+    // Set the viewport size.
     glViewport(left, 0, w, height); 
-    Screen::Resize(window);
+    // Record the new window dimensions in Screen.
+    Screen::recordNewWindowDimensions(window);
 }
