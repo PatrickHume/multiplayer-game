dcd1f83a6baa2f89440bcb6477fc52e92aa3ed74
PatrickHume <patrick.hume31@gmail.com>
2022-10-21 16:40:55 +0100

    docs: comment image creation in heightmap

--- source/heightmap.cpp
+++ source/heightmap.cpp
@@ -67,31 +67,30 @@ Heightmap::Heightmap(const char *filename)
     glPatchParameteri(GL_PATCH_VERTICES, NUM_PATCH_PTS);
 
     // ---------------------------------- Generating Buffers ----------------------------------
-
+    // Create the framebuffers.
     glGenRenderbuffers( 1, &renderbufId );
     glGenRenderbuffers( 1, &depthbufId );
     glGenFramebuffers ( 1, &framebufId   );
-
+    // Set the width and height to be that of the image, use only one color channel.
     glBindRenderbuffer(GL_RENDERBUFFER, renderbufId);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_R32F, width, height);
-
+    // Do the same for the depth buffer - Despite it not being used, I have not been able to
+    // successfully compile the framebuffer without having attached a depth buffer.
     glBindRenderbuffer(GL_RENDERBUFFER, depthbufId);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT32, width, height);
-    // Set up the framebuffer to hold the renderbuffers.
+    // Set up the framebuffer to hold both renderbuffers.
     glBindFramebuffer(GL_FRAMEBUFFER, framebufId);  
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_RENDERBUFFER, renderbufId);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,  GL_RENDERBUFFER, depthbufId  );
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,  GL_RENDERBUFFER, depthbufId);
 
     // Check frame buffer for errors.
     GLenum stat = glCheckFramebufferStatus(GL_FRAMEBUFFER);  
     if(stat != GL_FRAMEBUFFER_COMPLETE) { 
         throw std::runtime_error("Heightmap framebuffer is not framebuffer complete.");
         }
-    // Set up color attachment to draw buffer 1.
+    // Set up color attachment to draw buffer 0.
     const GLenum att[] = {GL_COLOR_ATTACHMENT2};
     glDrawBuffers(1, att);
-    // Bind the frame buffer back to 0 for regular drawing to the screen.
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     // Create shader that will write to the framebuffer.
     setupShader = std::make_shared<Shader>(
@@ -101,44 +100,39 @@ Heightmap::Heightmap(const char *filename)
         "resources/shaders/tesselation/controlSetup.tcs", 
         "resources/shaders/tesselation/evaluationSetup.tes"); 
 
-    glm::mat4 ortho = glm::ortho(0.0f, 1.0f, 0.0f, 1.0f, -2.0f, 2.0f);
-
-    glBindFramebuffer(GL_FRAMEBUFFER, framebufId);
-    std::cout << "glBindFramebuffer: " << glGetError() << std::endl;
-
+    // Resize the viewport to fit the image dimensions.
     glViewport(0,0,width,height);
     glDisable(GL_DEPTH_TEST);
     glClearColor(1.0,1.0,1.0,1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    std::cout << "glClearColor: " << glGetError() << std::endl;
-
+    // Activate the shader.
     setupShader->use();
+    // Set the projection matrix to be orthogonal, this will make the resultant image flat.
+    glm::mat4 ortho = glm::ortho(0.0f, 1.0f, 0.0f, 1.0f, -2.0f, 2.0f);
     setupShader->setMat4("projection",ortho);
+    // Bind the heightmap texture.
     texture->Bind();
     texture->texUnit(setupShader, "heightMap");
     glBindVertexArray(terrainVAO);
     glDrawArrays(GL_PATCHES, 0, 4*rez*rez);
     texture->Unbind();
-
+    // Wait for the draw to finish before reading the framebuffer.
     glFinish();
 
+    // Create a buffer to store frame buffer pixel values.
     GLfloat* floatBuffer = new GLfloat[width * height];
     glReadBuffer(GL_COLOR_ATTACHMENT2);
-    std::cout << "glReadBuffer: " << glGetError() << std::endl;
+    // Set the data to be 1 byte aligned.
     glPixelStorei(GL_PACK_ALIGNMENT, 1);
+    // Read the red value of each pixel, as a float, into floatBuffer.
     glReadPixels(0, 0, width, height, GL_RED, GL_FLOAT, floatBuffer);
-    std::cout << "glReadPixels: " << glGetError() << std::endl;
 
+    // Convert the floats into bytes for writing to png.
     unsigned char* charBuffer = new unsigned char[width * height * 4];
-
-    //for(int i = 0; i < width * height; i++){
-    //    std::cout << floatBuffer[i] << std::endl;    
-    //}
-
     for(int i = 0; i < width * height; i++){
+        // Flip the data upside down to match the original image orientation.
         charBuffer[i] = (unsigned char)(floatBuffer[(width * height) - 1 - i]);
     }
-
     stbi_write_png("image.png", width, height, 1, charBuffer, width);
 
     //exit(0);
